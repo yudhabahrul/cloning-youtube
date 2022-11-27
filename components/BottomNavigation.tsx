b811a00cd1de2d360000860e0bdfd09e0b94bfc3
yudhabahrul <yudhabahrul40@gmail.com>
2022-11-27 09:20:27 +0700

    fix ui 7

--- components/BottomNavigation.tsx
+++ components/BottomNavigation.tsx
@@ -7,23 +7,31 @@ import { ImFilePlay } from "react-icons/im";
 const BottomNavigation = () => {
   return (
     <nav className="fixed z-30 bottom-0 w-full space-x-7 px-8 justify-center bg-[#222222] flex items-center py-1 scale-0 md:scale-100">
-      <Link className="flex flex-col items-end" href="/">
-        <AiFillHome className="fill-white text-[1.35rem] mb-0.5" />{" "}
+      <Link className="flex flex-col items-center space-y-0.5" href="/">
+        <AiFillHome className="fill-white text-[1.35rem]" />{" "}
         <span className="text-white text-[10.3px]">Beranda</span>
       </Link>
-      <Link className="flex flex-col items-center" href="/">
-        <SiBbciplayer className="fill-white text-[1.2rem] mb-0.5" />{" "}
+      <Link className="flex flex-col items-center space-y-1" href="/">
+        <SiBbciplayer className="fill-white text-[1.2rem]" />{" "}
         <span className="text-white text-[10.3px]">Shorts</span>
       </Link>
       <Link className="" href="/">
         <BsPlusCircle className="fill-slate-100 text-3xl -mt-1.5" />{" "}
       </Link>
-      <Link title="Subsription" className="flex flex-col items-center" href="/">
-        <BsCollectionPlay className="fill-white text-[1.35rem] mb-0.5" />{" "}
+      <Link
+        title="Subsription"
+        className="flex flex-col items-center space-y-0.5"
+        href="/"
+      >
+        <BsCollectionPlay className="fill-white text-[1.35rem]" />{" "}
         <span className="text-white text-[10.3px]">Subscription</span>
       </Link>
-      <Link title="Koleksi" className="flex flex-col items-center" href="/">
-        <ImFilePlay className="fill-white text-[1.3rem] mb-0.5" />{" "}
+      <Link
+        title="Koleksi"
+        className="flex flex-col items-center space-y-0.5"
+        href="/"
+      >
+        <ImFilePlay className="fill-white text-[1.3rem]" />{" "}
         <span className="text-white text-[10.3px]">Koleksi</span>
       </Link>
     </nav>
